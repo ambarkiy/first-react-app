852914f5edeac47f6cc0e341e9d7899fb02d4036
ambarkiy <ambarki.youssef@gmail.com>
2019-12-03 04:28:28 +0100

    ma première application react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	var nom = 'Ambarki',
+		prenom = 'Youssef',
+		welcomeMessage = 'Bonjour',
+		fullMessage = `${welcomeMessage} ${nom} ${prenom}`;
+
+	return <h2>{fullMessage}</h2>;
 }
 
 export default App;
