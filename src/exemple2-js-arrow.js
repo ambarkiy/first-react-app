0a8e2973fc1500e961f2b88b10e4db53b4df3220
ambarkiy <ambarki.youssef@gmail.com>
2019-12-05 00:42:37 +0100

    rename file, delete unused exemple

--- src/exemple2-js-arrow.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const walou = () => {};
-const oneArgument = (arg1) => {};
-const oneArgumentBis = (arg1) => {};
-//const notAlowed = arg1,arg2 =>{};
-const allowed = (arg1, arg2) => {};
